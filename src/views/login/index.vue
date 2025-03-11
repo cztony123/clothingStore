dc3049cb9517cba55cca340ecfdf8e2f08aacd64
Your Name <czintony1314@gmail.com>
2025-03-11 23:35:57 +0800

    test

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="layout-box">
-        <!-- 登录注册登录 -->
+        <!-- 登录注册 -->
         <div class="content">
             <div class="logo">
                 <img src="../../assets/logo.png" alt="logo">
